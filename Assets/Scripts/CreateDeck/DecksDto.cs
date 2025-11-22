03cf7b9ae793951c52d704b4442ce01890514a03
Rui Teixeira <ruipedroalmeidateixeira@gmail.com>
2025-11-22 02:06:26 +0000

    feat(deck): Deck creation scene logic complete
    Missing just integration

---
+++ Assets/Scripts/CreateDeck/DecksDto.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.CreateDeck
+{
+    [System.Serializable]
+    public class DecksDto 
+    {
+        public int id;
+        public string name;
+        public List<DeckCards> cards;
+    }
+}
