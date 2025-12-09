f2f30fd48c951dadf6bb5b3140cecaeb240483d4
Pedro Campião <pedro.campiao1@gmail.com>
2025-12-09 01:03:54 +0000

    Fix bug on a functions that only works on the Unity Editor when building the project's executable

--- Assets/Colorblindness/Scripts/Colorblindness.cs
+++ Assets/Colorblindness/Scripts/Colorblindness.cs
@@ -49,6 +49,8 @@ namespace SOHNE.Accessibility.Colorblindness
 
         public static Colorblindness Instance { get; private set; }
 
+#if UNITY_EDITOR
+
         [UnityEditor.Callbacks.DidReloadScripts]
         private static void OnScriptsReloaded()
         {
@@ -56,6 +58,7 @@ namespace SOHNE.Accessibility.Colorblindness
             Debug.LogError("There is no type of <b>SRP</b> included in this project.");
 #endif
         }
+#endif
 
         void Awake()
         {
